e5a7262802bfc3e7f9b4b9bb216d2782c62a0ddc
Bill Plantz <wplantz@gmail.com>
2020-01-21 21:56:31 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by William Plantz on 1/21/20.
+//  Copyright © 2020 Bill Plantz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
